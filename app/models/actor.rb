cf77a3621317056a2ad16f6542dd8a6ff70e14af
SeanQuinn <Seanquinn917@gmail.com>
2023-10-11 11:10:59 -0400

    commit tree d566f9146c43b9a41f73bb0fcd21e7b76ccd3189

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name+" "+self.last_name
+  end 
+
+  def list_roles
+    self.shows.map do |show|
+      show.characters.map do |character|
+        "#{character.name} - #{show.name}"
+      end
+    end.flatten
+  end  
+
+
 end
